ead901eb3dc0adb26ec5770f9d1f269ac8b4cd5e
T45K <tasktas9@gmail.com>
2021-06-15 21:37:39 +0900

    Request Param validation

--- src/main/java/io/github/t45k/spring/trial/controller/TrialController.java
+++ src/main/java/io/github/t45k/spring/trial/controller/TrialController.java
@@ -1,9 +1,13 @@
 package io.github.t45k.spring.trial.controller;
 
+import io.github.t45k.spring.trial.annotation.Bigger;
 import io.github.t45k.spring.trial.request.Request;
+import lombok.Value;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,5 +20,22 @@ public class TrialController {
         System.out.println(request.getId1());
         System.out.println(request.getId2());
         return Map.of("success", true);
+    }
+
+    /*
+    Check request param
+     */
+    @GetMapping
+    public Map<String, Boolean> index(@Validated final RequestParams params) {
+        System.out.println(params.getMin());
+        System.out.println(params.getMax());
+        return Map.of("success", true);
+    }
+
+    @Value
+    @Bigger(max = "max", min = "min")
+    private static class RequestParams {
+        int min;
+        int max;
     }
 }
